c144968b8bfb52cbcf811624d0b2d7c52c887841
Mohammad-Hakemi22 <60274046+Mohammad-Hakemi22@users.noreply.github.com>
2020-07-26 19:01:36 +0430

    user profile

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,7 @@
 
 from django.contrib.auth import views
 from django.urls import path
-from .views import ArticleList,ArticleCreate,ArticleUpdate,ArticleDelete
+from .views import ArticleList,ArticleCreate,ArticleUpdate,ArticleDelete,Profile
 
 app_name = 'account'
 
@@ -23,4 +23,5 @@ urlpatterns += [
     path('article/create', ArticleCreate.as_view(), name='article-create'),
     path('article/update/<int:pk>', ArticleUpdate.as_view(), name='article-update'),
     path('article/delete/<int:pk>', ArticleDelete.as_view(), name='article-delete'),
+    path('profile/', Profile.as_view(), name='profile'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import ListView, CreateView,UpdateView,DeleteView
+from django.views.generic import ListView, CreateView, UpdateView, DeleteView
 from web.models import Articles
 from django.urls import reverse_lazy
-from .mixins import FieldsMixins, FormValidMixins,AuthorAccessMixins,SuperUserDeleteMixins
+from .mixins import FieldsMixins, FormValidMixins, AuthorAccessMixins, SuperUserDeleteMixins
+from .models import User
 
 
 class ArticleList(LoginRequiredMixin, ListView):
@@ -27,7 +28,17 @@ class ArticleUpdate(AuthorAccessMixins, FieldsMixins, FormValidMixins, UpdateVie
     template_name = "registration/article-create-update.html"
 
 
-class ArticleDelete(SuperUserDeleteMixins,DeleteView):
+class ArticleDelete(SuperUserDeleteMixins, DeleteView):
     model = Articles
     success_url = reverse_lazy('account:home')
-    template_name="registration/articles_confirm_delete.html"
+    template_name = "registration/articles_confirm_delete.html"
+
+
+class Profile(UpdateView):
+    model = User
+    fields = ['username', 'email', 'first_name',
+              'last_name', 'special_user', 'is_author']
+    template_name = "registration/profile.html"
+    success_url=reverse_lazy('account:profile')
+    def get_object(self):
+        return User.objects.get(pk=self.request.user.pk)
