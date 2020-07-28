a8bb46b67635d28d2b3149e2392d0d24daf8b694
Mohammad-Hakemi22 <60274046+Mohammad-Hakemi22@users.noreply.github.com>
2020-07-28 17:02:13 +0430

    Fix bug -> login users

--- account/mixins.py
+++ account/mixins.py
@@ -51,7 +51,10 @@ class SuperUserDeleteMixins():
 
 class AuthorsAccessMixins():
     def dispatch(self, request, *args, **kwargs):
-        if request.user.is_superuser or request.user.is_author:
-            return super().dispatch(request, *args, **kwargs)
+        if request.user.is_authenticated:
+            if request.user.is_superuser or request.user.is_author:
+                return super().dispatch(request, *args, **kwargs)
+            else:
+                return redirect("account:profile")
         else:
-            return redirect("account:profile")
+            return redirect("account:login")
